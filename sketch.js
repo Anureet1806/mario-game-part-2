d4b87af1ebe1df7608f3995a65863b4d66371970
Anureet1806 <73575436+Anureet1806@users.noreply.github.com>
2021-01-25 10:41:01 +1100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,30 +4,31 @@ var clouds, cloudsimg;
 var invisibleground;
 var enemies,enemyimg;
 var pipesGroup,cloudsGroup;
-var enemiesGroup;
-
-
-
+var PLAY =1;
+var END = 0;
+var gameState = PLAY;
+var score = 5;
+var count = 0;
+var coincount = 0;
+var instruct;
 function preload(){
-  //bg = loadImage("images/bg.png");
+  bg = loadImage("images/bg.png");
   groundimg = loadImage("images/ground.png");
   marioimg = loadAnimation("images/mario1.png","images/mario2.png");
   pipesimg =loadImage("images/pipes.png");
   cloudsimg =loadImage("images/cloud.png");
-  //mario_deadimg = loadAnimation("images/mario_dead.png");
-  //gameoverimg = loadImage("images/gameOver.png");
-  //restartImg = loadImage("images/restart.png");
-  //bulletimg = loadImage("images/bullet.png");
+  mario_deadimg = loadAnimation("images/mario_dead.png");
+  gameoverimg = loadImage("images/gameOver.png");
+  restartImg = loadImage("images/restart.png");
+  bulletimg = loadImage("images/bullet.png");
   enemyimg =loadAnimation("images/enemy1.png","images/enemy2.png");
-	//enemy_dieimg =loadAnimation("images/enemy1.png");
-	//marioheadimg = loadImage("images/mario-head.png");
-	//coinimg = loadImage("images/coin.png");
-	//textimg = loadImage("images/text.png");
+	enemy_dieimg =loadAnimation("images/enemy1.png");
+	marioheadimg = loadImage("images/mario-head.png");
+	coinimg = loadImage("images/coin.png");
+	textimg = loadImage("images/text.png");
 }
 
-function setup(){
-  createCanvas(1200,400);
-
+function setup() { 
   createCanvas(1200, 400);
   ground = createSprite(600,390,1200,10);
   ground.addImage("ground",groundimg);
@@ -35,47 +36,106 @@ function setup(){
 
   mario = createSprite(50,335,10,10);
   mario.addAnimation("mario",marioimg);
-  //mario.addAnimation("mario_dead",mario_deadimg);
+  mario.addAnimation("mario_dead",mario_deadimg);
   mario.scale =0.3;
   
 
   invisibleground = createSprite(600,375,1200,10);
   invisibleground.visible = false;
 
-  cloudsGroup= new Group();
-
-  enemiesGroup= new Group();
  
-  pipesGroup=new Group();
+  gameOver = createSprite(620,150);
+  gameOver.addImage("gameover",gameoverimg);
+  gameOver.scale = 0.5;
+
+  restart = createSprite(1100,40);
+  restart.addImage("restart",restartImg);
+  restart.scale =0.15;
+
+  gameOver.visible = false;
+  restart.visible = false;
+
+  pipesGroup = new Group();
+  cloudsGroup = new Group();
+  bulletGroup = new Group();
+	enemyGroup = new Group();
+	coinGroup = new Group();
+
+	mariohead = createSprite(50,50,10,10);
+	mariohead.addImage("mariohead",marioheadimg);
+	mariohead.scale= 1.5;
+	
+	coin = createSprite(200,50,10,10);
+	coin.addImage("coin",coinimg);
+
+	instruct = createSprite(600,170);
+	instruct.addImage("instruct",textimg);
+	instruct.lifetime =200;
+
 
 }
 
-function draw(){ 
-  background("blue");
-
-
-
+function draw() {
+  background("skyblue");
   drawSprites();
+  
+  fill ("green");
+  textSize(35);
+  text(score,120,60)
+  
+  if(gameState===PLAY){
+    ground.velocityX=-7;
+    count=count+0.1;
 
-  ground.velocityX=-7;
+    if(ground.x<0){
+      ground.x = ground.width/2;
+    }
+  
+    if(keyDown("UP_ARROW") && mario.y>329){ 
+      mario.velocityY = -20;
+    }
 
-  if(ground.x<0){
-    ground.x = ground.width/2;
-  }
+    if(keyWentDown("space")){
+      bullet=createSprite(mario.x,mario.y);
+      bullet.addImage("bullet",bulletimg);
+      bullet.velocityX=4;
+      bulletGroup.add(bullet);
+    }
 
-  if(keyDown("UP_ARROW") && mario.y>329){ 
-    mario.velocityY = -10;
-  }
-
-  mario.velociyY=mario.velocityY+1;
+ mario.velocityY=mario.velocityY+1;
   spawnPipes();
-
-  mario.collide(invisibleground);
-
   spawnClouds();
-
+  spawnCoins();
   spawnEnemies();
+  
+ if(pipesGroup.isTouching(mario)){
+   score=score-1;
+   count=count-5;
+   gameState=END;
+ }
+
+ if(enemyGroup.isTouching(mario)){
+  score=score-1;
+  count=count-5;
+  gameState=END;
+ }
+
+ if(bulletGroup.isTouching(enemyGroup)){
+   enemyGroup.destroyEach();
+   bulletGroup.destroyEach();
+ }
+  
 }
+
+else if(gameState===END){
+  ground.velocityX=0;
+  mario.velocityX=0;
+  
+}
+mario.collide(invisibleground);
+console.log(mario.y);
+}
+
 
 
 function spawnPipes(){
@@ -89,26 +149,62 @@ function spawnPipes(){
   
   }
 }
-
 function spawnClouds(){
-  if(frameCount%100===0){
-  clouds=createSprite(1200,random(50,100),10,10);
-  clouds.addImage("clouds",cloudsimg);
-  clouds.velocityX=-3;
-  clouds.scale=2;
-  clouds.lifetime=400;
-  cloudsGroup.add(clouds);
+  if(frameCount %100  === 0){
+    clouds = createSprite(1200,random(50,150),10,10);
+    clouds.addImage("clouds",cloudsimg);
+    clouds.velocityX = -3;
+    clouds.scale = 2;
+    clouds.lifetime = 420;
+    cloudsGroup.add(clouds);
+  }
+}
+function spawnEnemies(){
+  if(frameCount % 300 === 0){
+    enemies = createSprite(1200,330,10,20);
+    enemies.addAnimation("enemy",enemyimg);
+    enemies.scale=0.15;
+    enemies.velocityX =-6;
+    enemies.lifetime = 200;
+    enemyGroup.add(enemies);
+  }
+  
+}
+function reset(){
+  gameState = PLAY;
+  pipesGroup.destroyEach();
+  cloudsGroup.destroyEach();
+	enemyGroup.destroyEach();
+	coinGroup.destroyEach();
+  gameOver.visible = false;
+	restart.visible = false;
+	
+	mario.changeAnimation("mario",marioimg);
+	
+}
+
+function spawnCoins(){
+	if(frameCount%200 === 0){
+		for(var i=0 ; i<5 ;i++){
+			coin = createSprite(1200+i*20,200 ,10,10);
+			coin.addImage("coin",coinimg);
+			coin.velocityX = -4;
+			coin.lifetime = 1000;
+			coinGroup.add(coin);
+		}
+	}
+}
+
+function spawnCoins(){
+  if(frameCount%200===0){
+    for(var i=0;i<5;i++){
+      coin=createSprite(1200+i*20,200,10,10);
+      coin.addImage("coin",coinimg);
+      coin.velocityX=-4;
+      coin.lifetime=1000;
+      coinGroup.add(coin);
+    }
   }
 }
 
-function spawnEnemies(){
-  if(frameCount%300===0){
-  enemies=createSprite(1200,330,10,30);
-  enemies.addImage("enemies",enemyimg);
-  enemies.velocityX=-3;
-  enemies.scale=2;
-  enemies.lifetime=200;
-  enemiesGroup.add(enemies);
-  }
-}
 
